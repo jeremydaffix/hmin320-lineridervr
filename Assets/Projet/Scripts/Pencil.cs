a1a6ec225e4daa12c00c9ae3dd0954ada91a892a
gg <jdbasic@gmail.com>
2018-12-16 00:28:44 +0100

    poser ses boules

---
+++ Assets/Projet/Scripts/Pencil.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pencil : MonoBehaviour {
+
+    public GameObject BallPrefab;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void CreateBall()
+    {
+        Debug.Log("CREATING BALL");
+        GameObject b = GameObject.Instantiate(BallPrefab, transform.position, new Quaternion());
+        b.transform.localPosition += new Vector3(0f, 0.15f, 0f);
+    }
+}
